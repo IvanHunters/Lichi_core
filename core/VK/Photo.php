12dbb7a0ab0aafc481e36ca4f75fad9f135f9b26
ohotnikov.ivan <ohotnikov.ivan@e-queo.net>
2024-05-28 17:25:56 +0300

    add core

--- core/VK/Photo.php
+++ core/VK/Photo.php
@@ -13,12 +13,6 @@ trait Photo{
         if($flag_user) $method = "CallHowGroup";
         else       $method = "CallHowUser";
 
-        $redis = \Cache::store('redis');
-
-//        $attach = $redis->get($file);
-//        if($attach && $flag_person == false)
-//          return $attach;
-
         if($flag_person)
         {
           $params_photo = ['peer_id'=>$this->user_id];
@@ -42,14 +36,6 @@ trait Photo{
 //          $upload_url = $upload_url['upload_url'];
         }
 
-
-        $handle = $redis->get("handle");
-        if (is_null($handle)) {
-            $handle = [];
-        }
-        $handle[$this->user_id]['log'][] = "Получил от вк сервер для загрузки " . $file;
-        $redis->set("handle", $handle);
-
         $aPost = array(
             'photo' => new \CURLFile($file)
         );
@@ -63,67 +49,24 @@ trait Photo{
         $result = curl_exec($ch);
         $res = json_decode($result, true);
 
-        $handle = $redis->get("handle");
-        if (is_null($handle)) {
-            $handle = [];
-        }
-        $handle[$this->user_id]['log'][] = "Загрузил и проверяю";
-        $redis->set("handle", $handle);
-
         if (empty($res) || empty($res['photo'])) {
-            $handle = $redis->get("handle");
-            if (is_null($handle)) {
-                $handle = [];
-            }
-            $handle[$this->user_id]['log'][] = "Не удалось загрузить " . $result;
-            $redis->set("handle", $handle);
-
             $errors[] = sprintf("params_for_getting_upload_server: %s\nupload_response: %s\nupload_url: %s\nresponse: %s", json_encode($params_photo, true), json_encode($uploadData, true), $upload_url, $result);
             $this->upload_photo($file, $flag_user, $flag_person, ($retry+1), $errors);
         }
         curl_close($ch);
 
-
-        $handle = $redis->get("handle");
-        if (is_null($handle)) {
-            $handle = [];
-        }
-        $handle[$this->user_id]['log'][] = "Все ок, сохраняю в базе вк аттач " . var_export($res, true);
-        $redis->set("handle", $handle);
-
         $attachment = [];
         try {
             $attachment = $this->{$method}("photos.saveMessagesPhoto", array("photo" => $res['photo'], "server" => $res['server'], "hash" => $res['hash']));
         } catch (\Throwable $e) {
-            $handle = $redis->get("handle");
-            if (is_null($handle)) {
-                $handle = [];
-            }
-            $handle[$this->user_id]['log'][] = "Не удалось загрузить " . $e->getMessage();
-            $redis->set("handle", $handle);
-
             $errors = sprintf("params_for_getting_upload_server: %s\nupload_response: %s\nupload_url: %s\nresponse: %s", json_encode($params_photo, true), json_encode($uploadData, true), $upload_url, $result);
             throw new \RuntimeException($errors);
         }
-
-        $handle = $redis->get("handle");
-        if (is_null($handle)) {
-            $handle = [];
-        }
-        $handle[$this->user_id]['log'][] = "Все ок, Аттачей " . count($attachment);
-        $redis->set("handle", $handle);
         
         if(isset($attachment[0]))
         {
           $attachment = $attachment[0];
         }else{
-            $handle = $redis->get("handle");
-            if (is_null($handle)) {
-                $handle = [];
-            }
-            $handle[$this->user_id]['log'][] = "Не удалось загрузить " . var_export($attachment, true);
-            $redis->set("handle", $handle);
-            
             $errors = sprintf("params_for_getting_upload_server: %s\nupload_response: %s\nupload_url: %s\nresponse: %s", json_encode($params_photo, true), json_encode($uploadData, true), $upload_url, $result);
             throw new \RuntimeException($errors);
             //$this->upload_photo($file, $flag_user, $flag_person, ($retry+1), $errors);
@@ -135,14 +78,6 @@ trait Photo{
         }
 
         $attachment_vk = "photo".$attachment['owner_id']."_".$attachment['id'];
-
-
-        $handle = $redis->get("handle");
-        if (is_null($handle)) {
-            $handle = [];
-        }
-        $handle[$this->user_id]['log'][] = "Загрузил " . $attachment_vk . " " . $retry;
-        $redis->set("handle", $handle);
 
         return $attachment_vk;
     }
